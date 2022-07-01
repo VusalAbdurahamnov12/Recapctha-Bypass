1842e6505100e4381127f659347867068073d311
test <sh13ld001@gmail.com>
2022-07-01 16:06:13 +0400

    add TamperMonkey

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+// ==UserScript==
+// @name         Recaptcha Solver BySh13ld
+// @author       engageub
+// @match        *://*/recaptcha/*
+// @connect      engageub.pythonanywhere.com
+// @connect      engageub1.pythonanywhere.com
+// @grant        GM_xmlhttpRequest
+/*
+
+/** Note: This script is solely intended for the use of educational purposes only and not to abuse any website.
+This script uses audio in order to solve the captcha. Use it wisely and do not abuse any website.
+*/
+// ==/UserScript==
 (function() {
     'use strict';
     var solved = false;
